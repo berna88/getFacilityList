fd1bd9a4147a2b144799111643ba6f8ef352f889
Bernardo <bhernandez@consistent.com.mx>
2019-05-30 18:27:56 -0500

    Ajueste en validacion

--- src/main/java/com/consistent/facility/configuration/ConfigurationImpl.java
+++ src/main/java/com/consistent/facility/configuration/ConfigurationImpl.java
@@ -34,49 +34,49 @@ public class ConfigurationImpl {
 		try{
 			if(configuration!=null){
 				//Facility
-				if(configuration.getFacilityId()!=null){
+				if(configuration.getFacilityId()!=0){
 					Constants.FACILITY_ID = configuration.getFacilityId();
 					log.info("Configuration Facility_Id "+Constants.FACILITY_ID);
 				}else{
 					log.info("Configuration Facility_Id is null "+Constants.FACILITY_ID);
 				}
 				//Restaurant
-				if(configuration.getRestaurantId()!=null){
+				if(configuration.getRestaurantId()!=0){
 					Constants.RESTAURANTE_ID = configuration.getRestaurantId();
 					log.info("Configuration Restaurant_Id "+Constants.RESTAURANTE_ID);
 				}else{
 					log.info("Configuration Restaurant_Id is null "+Constants.RESTAURANTE_ID);
 				}
 				//Meeting Room
-				if(configuration.getMeetingRoomId()!=null){
+				if(configuration.getMeetingRoomId()!=0){
 					Constants.MEETING_ROOM_ID = configuration.getMeetingRoomId();
 					log.info("Configuration Meeting_Room_Id "+Constants.MEETING_ROOM_ID);
 				}else{
 					log.info("Configuration Meeting_Room_Id is null "+Constants.MEETING_ROOM_ID);
 				}
 				//FiestaKidsClub
-				if(configuration.getFiestaKidsClubId()!=null){
+				if(configuration.getFiestaKidsClubId()!=0){
 					Constants.FIESTA_KIDS_CLUB_ID = configuration.getFiestaKidsClubId();
 					log.info("Configuration FiestaKidsClub_Id "+Constants.FIESTA_KIDS_CLUB_ID);
 				}else{
 					log.info("Configuration FiestaKidsClub_Id is null "+Constants.FIESTA_KIDS_CLUB_ID);
 				}
 				//Spa
-				if(configuration.getSpaId()!=null){
+				if(configuration.getSpaId()!=0){
 					Constants.SPA_ID = configuration.getSpaId();
 					log.info("Configuration Spa_Id "+Constants.SPA_ID);
 				}else{
 					log.info("Configuration Spa_Id is null "+Constants.SPA_ID);
 				}
 				//Gym
-				if(configuration.getGymId()!=null){
+				if(configuration.getGymId()!=0){
 					Constants.GYM_ID = configuration.getGymId();
 					log.info("Configuration Gym_Id "+Constants.GYM_ID);
 				}else{
 					log.info("Configuration Gym_Id is null "+Constants.GYM_ID);
 				}
 				//Bar
-				if(configuration.getBarId()!=null){
+				if(configuration.getBarId()!=0){
 					Constants.BAR_ID = configuration.getBarId();
 					log.info("Configuration Bar_Id "+Constants.BAR_ID);
 				}else{
--- src/main/java/com/consistent/facility/interfaces/Configuration.java
+++ src/main/java/com/consistent/facility/interfaces/Configuration.java
@@ -7,24 +7,24 @@ import aQute.bnd.annotation.metatype.Meta;
 @Meta.OCD( localization = "content/Language",id = "com.consistent.facility.interfaces.Configuration",name = "Service FacilityList")
 public interface Configuration {
 	
-	@Meta.AD(required = false,description = "Get Struture Restaurant id")
+	@Meta.AD(required = false,description = "Get Struture Restaurant id", deflt="0")
     public Long getRestaurantId();
 	
-	@Meta.AD(required = false,description = "Get Struture Facility id")
+	@Meta.AD(required = false,description = "Get Struture Facility id", deflt="0")
     public Long getFacilityId();
 	
-	@Meta.AD(required = false,description = "Get Struture Meeting Room id")
+	@Meta.AD(required = false,description = "Get Struture Meeting Room id", deflt="0")
     public Long getMeetingRoomId();
 	
-	@Meta.AD(required = false,description = "Get Struture FiestaKidsClub id")
+	@Meta.AD(required = false,description = "Get Struture FiestaKidsClub id", deflt="0")
     public Long getFiestaKidsClubId();
 	
-	@Meta.AD(required = false,description = "Get Struture Spa id")
+	@Meta.AD(required = false,description = "Get Struture Spa id", deflt="0")
     public Long getSpaId();
 	
-	@Meta.AD(required = false,description = "Get Struture Gym id")
+	@Meta.AD(required = false,description = "Get Struture Gym id", deflt="0")
     public Long getGymId();
 	
-	@Meta.AD(required = false,description = "Get Struture Bar id")
+	@Meta.AD(required = false,description = "Get Struture Bar id", deflt="0")
     public Long getBarId();
 }
